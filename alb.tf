38c3a4b95764682785c7445fb3d01d851395d55f
Mike Dettmer <mikedett91@gmail.com>
2022-03-23 12:23:55 -0400

    ALB bug fixes

--- alb.tf
+++ alb.tf
@@ -1,13 +1,13 @@
 # Creating External LoadBalancer
 resource "aws_lb" "external-alb" {
-  name               = "External LB"
+  name               = "External-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.three-tier-sg.id]
-  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
-resource "aws_lb_target_group" "target-elb" {
-  name     = "ALB TG"
+resource "aws_lb_target_group" "external-alb" {
+  name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.three-tier-vpc.id
@@ -25,11 +25,11 @@ resource "aws_lb_target_group_attachment" "attachment1" {
   target_id        = aws_instance.three-tier-instance1.id
   port             = 80
   depends_on = [
-    aws_instance.three-three-tier-instance1,
+    aws_instance.three-tier-instance1,
   ]
 }
 resource "aws_lb_listener" "external-elb" {
-  load_balancer_arn = aws_lb.external-elb.arn
+  load_balancer_arn = aws_lb.external-alb.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
