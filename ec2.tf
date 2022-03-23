6b5d6e9b06c3ff6026517f43d3314f40e9dfe63d
Mike Dettmer <mikedett91@gmail.com>
2022-03-23 12:19:59 -0400

    Remove count from ec2, caused problems

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "three-tier-instance" {
   ami                         = "ami-087c17d1fe0178315"
   instance_type               = "t2.micro"
-  count                       = 1
+#  count                       = 1
   key_name                    = "tests"
   vpc_security_group_ids      = ["${aws_security_group.three-tier-sg.id}"]
   associate_public_ip_address = true
@@ -16,7 +16,7 @@ resource "aws_instance" "three-tier-instance" {
 resource "aws_instance" "three-tier-instance1" {
   ami                         = "ami-087c17d1fe0178315"
   instance_type               = "t2.micro"
-  count                       = 1
+#  count                       = 1
   key_name                    = "tests"
   vpc_security_group_ids      = ["${aws_security_group.three-tier-sg.id}"]
   associate_public_ip_address = true
