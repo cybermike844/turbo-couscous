4d06ac32c6c104235296385a96e20635eaf91f58
Mike Dettmer <mikedett91@gmail.com>
2022-03-23 12:17:27 -0400

    Bug fixin

--- web_sg.tf
+++ web_sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "three-tier-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress = {
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
