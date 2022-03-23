00fb754921e39ecf9ad9144dd7a5a17ded653d76
Mike Dettmer <mikedett91@gmail.com>
2022-03-22 20:22:22 -0400

    alb setup

--- alb.tf
+++ alb.tf
@@ -1,14 +1,39 @@
 # Creating External LoadBalancer
 resource "aws_lb" "external-alb" {
-  name = "External LB"
-  internal = false
+  name               = "External LB"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.three-tier-sg.id]
-  subnets = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2]
+  security_groups    = [aws_security_group.three-tier-sg.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2]
 }
 resource "aws_lb_target_group" "target-elb" {
-  name = "ALB TG"
-  port = 80
+  name     = "ALB TG"
+  port     = 80
   protocol = "HTTP"
-  vpc_id = aws_vpc.three-tier-vpc.id
+  vpc_id   = aws_vpc.three-tier-vpc.id
+}
+resource "aws_lb_target_group_attachment" "attachment" {
+  target_group_arn = aws_lb_target_group.external-alb.arn
+  target_id        = aws_instance.three-tier-instance.id
+  port             = 80
+  depends_on = [
+    aws_instance.three-tier-instance,
+  ]
+}
+resource "aws_lb_target_group_attachment" "attachment" {
+  target_group_arn = aws_lb_target_group.external-alb.arn
+  target_id        = aws_instance.three-tier-instance1.id
+  port             = 80
+  depends_on = [
+    aws_instance.three-three-tier-instance1,
+  ]
+}
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-elb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-alb.arn
+  }
 }
